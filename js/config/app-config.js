c0b3bf3db31cc6eb13e6697ad7e81c0d89efd47a
hrtechifyed <hrtechifyed@gmail.com>
2026-07-09 21:15:48 +0530

    Add application configuration for GrowWithHR

---
+++ js/config/app-config.js
@@ -0,0 +1,29 @@
+/**
+ * =============================================================================
+ * GrowWithHR
+ * Application Configuration
+ * =============================================================================
+ */
+
+const APP_CONFIG = Object.freeze({
+
+    productName: "GrowWithHR",
+
+    companyName: "HRTechify",
+
+    version: "0.9.0-beta",
+
+    releaseName: "Public Beta",
+
+    website:
+        "https://hrtechifyed.github.io/GrowwithHR/",
+
+    supportEmail:
+        "hrtechifyed@gmail.com",
+
+    copyright:
+        `© ${new Date().getFullYear()} HRTechify. All Rights Reserved.`
+
+});
+
+export default APP_CONFIG;
